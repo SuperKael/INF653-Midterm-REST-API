783df02ae023005d785158d89c856edc14ba7b65
Kael Haney <kaelhaney@gmail.com>
2022-03-02 22:55:44 -0500

    Test

--- config/database.php
+++ config/database.php
@@ -1,17 +1,17 @@
 <?php
     class Database {
 
-        private $connection = null;
+        private static $connection = null;
 
         public static function connect() {
-            if (is_null($this->connection)) {
-                $this->connection = new PDO(getenv("JAWSDB_URL"));
+            if (is_null(self::$connection)) {
+                self::$connection = new PDO(getenv("JAWSDB_URL"));
             }
-            return $this->connection;
+            return self::$connection;
         }
 
         public static function reset() {
-            $this->connection = null;
+            self::$connection = null;
         }
 
     }
